5e4ead7fff46711a4297d75b58bce5b20f6013d9
Danny Ha <danny.ha@genesys.com>
2026-02-10 16:10:56 -0500

    HUB: update moveItem debugging log 2

--- src/components/GridLayout.vue
+++ src/components/GridLayout.vue
@@ -528,9 +528,9 @@
                         break;
                     case 'up': {
                         const index = parseInt(elem.getAttribute('data-colsSortIndex'));
-                        console.log("&&&&&&&&&& MOVE ITEM UP1a :::", index, index > 0 ? (index - 1) : 0);
-                        console.log("&&&&&&&&&& MOVE ITEM UP2a :::", JSON.stringify(this.colsSort), l.x);
-                        const data = this.colsSort[l.x][index > 0 ? (index - 1) : 0];
+                        console.log("&&&&&&&&&& MOVE ITEM UP1b :::", index, index > 0 ? (index - 1) : 0);
+                        console.log("&&&&&&&&&& MOVE ITEM UP2b :::", JSON.stringify(this.colsSort), this.colsSort[(l.x).toString()], l.x);
+                        const data = this.colsSort[(l.x).toString()][index > 0 ? (index - 1) : 0];
                         y = data.y > 0 ? data.y - 1 : 0;
                         break;
                     }
